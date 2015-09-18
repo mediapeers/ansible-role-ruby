b501a3843dec956ce542281beb10f3b8583969c5
Stefan Horning <stefan@hornings.de>
2015-09-18 16:34:04 +0200

    Working install and linking of rubies. Tested with jruby and ruby (MRI).

--- spec/ruby_spec.rb
+++ spec/ruby_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe "Ruby installation" do
-  ruby_version = ANSIBLE_VARS.fetch('ruby_version', 'FAIL').split('-').last
-  if ANSIBLE_VARS.fetch('ruby_version', 'FAIL').include('-')
-    ruby_edition = ANSIBLE_VARS.fetch('ruby_version', 'FAIL').split('-').first
+  ruby_version = ANSIBLE_VARS.fetch('ruby_version_string', 'FAIL').split('-').last
+  if ANSIBLE_VARS.fetch('ruby_version_string', 'FAIL').include?('-')
+    ruby_edition = ANSIBLE_VARS.fetch('ruby_version_string', 'FAIL').split('-').first
   else
     ruby_edition = 'ruby'
   end
 
   describe package('build-essential') do
-    it { should exist }
+    it { should be_installed }
   end
 
   describe file('/opt/rubies') do
     it { should be_directory }
   end
 
-  describe file("/opt/rubies/#{ruby_version}") do
+  describe file("/opt/rubies/#{ruby_edition}-#{ruby_version}") do
     it { should be_directory }
   end
 
@@ -26,5 +26,14 @@ describe "Ruby installation" do
 
   describe command('gem -v') do
     its(:stdout) { should match /2.4/ }
+  end
+
+  # Should cleanup after itself
+  describe file("/usr/local/src/#{ruby_edition}-#{ruby_version}") do
+    it { should_not exist }
+  end
+
+  describe file("/usr/local/src/#{ruby_edition}-#{ruby_version}.tar.bz2") do
+    it { should_not exist }
   end
 end
